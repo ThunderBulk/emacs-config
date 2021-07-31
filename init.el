0989539873825be03726acf462dec0bc514356f3
ThunderBulk <jdhoey@icloud.com>
2021-07-31 15:42:19 +0100

    added customisation to org-refile

--- init.el
+++ init.el
@@ -178,6 +178,12 @@
 (setq org-startup-truncated nil)
 
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
+
+;; allow refile under sub-headings
+(setq org-refile-targets '((nil :maxlevel . 9)
+                                (org-agenda-files :maxlevel . 9)))
+(setq org-outline-path-complete-in-steps nil)         ; Refile in a single go
+(setq org-refile-use-outline-path t)                  ; Show full paths for refiling
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org-superstar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
