e89d8e4c11cc6269d89f723141ba7aba409b35e1
ThunderBulk <jdhoey@icloud.com>
2021-07-31 01:32:36 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,258 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Inspired by https://gist.github.com/nilsdeppe/7645c096d93b005458d97d6874a91ea9
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Tweaks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; turn on highlight matching brackets when cursor is on one
+(show-paren-mode t)
+;; Auto-wrap at 80 characters
+(setq-default auto-fill-function 'do-auto-fill)
+(setq-default fill-column 80)
+(turn-on-auto-fill)
+;; Disable auto-fill-mode in programming mode
+(add-hook 'prog-mode-hook (lambda () (auto-fill-mode -1)))
+;; Put all backups in their own dir
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
+;; Highlight the line we are currently on
+(global-hl-line-mode t)
+;; Disable the toolbar at the top since it's useless
+(if (functionp 'tool-bar-mode) (tool-bar-mode -1))
+
+;; MacOS specific keybinds
+(setq ns-option-modifier 'super)
+(setq ns-command-modifier 'meta)
+(setq ns-right-option-modifier nil)
+(setq ns-right-command-modifier nil)
+
+;; Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ivy config
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package ivy
+  :ensure t
+  :commands (ivy-mode)
+  :config
+  (require 'ivy)
+  (ivy-mode t)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (setq ivy-wrap t)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  ;; Show #/total when scrolling buffers
+  (setq ivy-count-format "%d/%d ")
+  )
+
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper))
+  )
+
+(use-package counsel
+  :ensure t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("<f1> f" . counsel-describe-function)
+         ("<f1> v" . counsel-describe-variable)
+         ("<f1> l" . counsel-find-library)
+         ("<f2> i" . counsel-info-lookup-symbol)
+         ("<f2> u" . counsel-unicode-char)
+         ("C-c g" . counsel-git-grep)
+         ("C-c j" . counsel-git)
+         ("C-c k" . counsel-ag)
+         ("C-c r" . counsel-rg)
+         ("C-x l" . counsel-locate)
+         :map minibuffer-local-map
+         ("C-r" . counsel-minibuffer-add)
+         )
+  :config
+  ;; (If (executable-find "rg")
+  ;;     ;; use ripgrep instead of grep because it's way faster
+  ;;     (setq counsel-grep-base-command
+  ;;           "rg -i -M 120 --no-heading --line-number --color never '%s' %s"
+  ;;           counsel-rg-base-command
+  ;;           "rg -i -M 120 --no-heading --line-number --color never %s ."
+  ;;           )
+  ;;   (warn "\nWARNING: Could not find the ripgrep executable. It "
+  ;;         "is recommended you install ripgrep.")
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Beacon-mode: flash the cursor when switching buffers or scrolling
+;;              the goal is to make it easy to find the cursor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package beacon
+  :ensure t
+  :init
+  (eval-when-compile
+    ;; Silence missing function warnings
+    (declare-function beacon-mode "beacon.el"))
+  :config
+  (beacon-mode t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; which-key: when you pause on a keyboard shortcut it provides
+;;            suggestions in a popup buffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package which-key
+  :ensure t
+  :init
+  (eval-when-compile
+    ;; Silence missing function warnings
+    (declare-function which-key-mode "which-key.el"))
+  :config
+  (which-key-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; avy: always fast jump to char inside the current view buffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package avy
+  :ensure t
+  :bind (("M-c" . avy-goto-char)
+         ("M-s" . avy-goto-word-1))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; deft: fast note taking
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package deft
+  :ensure t
+  :config
+  (setq deft-directory "/Users/jdhoey/Library/Mobile Documents/iCloud~md~obsidian/Documents/my-brain")
+  (setq deft-extensions '("org" "md" "txt"))
+(setq deft-default-extension "md")
+(setq deft-text-mode 'markdown-mode)
+(setq deft-recursive t)
+(setq deft-use-filename-as-title t)
+(setq deft-file-naming-rules '((nospace . "-")
+                               (case-fn . downcase)))
+(setq deft-auto-save-interval 0)
+(global-set-key [f8] 'deft)
+(setq deft-strip-summary-regexp
+      (concat "\\("
+              "[\n\t]" ;; blank
+              "\\|^#\\+[a-zA-Z_]+:.*$" ;;org-mode metadata
+              "\\)"))
+)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Magit
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package magit
+  :ensure t
+  :requires dash
+  :after (ivy)
+  :commands (magit-checkout)
+  :bind (("M-g M-s" . magit-status)
+         ("M-g M-c" . 'magit-checkout)
+         )
+  :config
+  (add-hook 'magit-mode-hook (lambda () (setq whitespace-mode -1)))
+  (setq magit-completing-read-function 'ivy-completing-read)
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Appearance
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Theme
+(use-package dracula-theme
+  :ensure t
+  :config
+  (load-theme 'dracula t))
+;; Hide the scroll bar
+(scroll-bar-mode -1)
+;; Set default window size and position
+(setq default-frame-alist
+      '((top . 0) (left . 0) ;; position
+        (width . 110) (height . 90) ;; size
+        ))
+;; Enable line numbers on the LHS
+(global-linum-mode 1)
+;; Set the font to size 16 (160/10)
+(set-face-attribute 'default nil :height 140)
+;; I don't care to see the splash screen
+(setq inhibit-splash-screen t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Powerline theme
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; powerline theme where the modes are on the right side.
+(use-package powerline
+  :ensure t
+  :config
+  (defun powerline-right-theme ()
+    "Setup a mode-line with major and minor modes on the right side."
+    (interactive)
+    (setq-default mode-line-format
+                  '("%e"
+                    (:eval
+                     (let* ((active (powerline-selected-window-active))
+                            (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                            (mode-line (if active 'mode-line 'mode-line-inactive))
+                            (face0 (if active 'powerline-active0 'powerline-inactive0))
+                            (face1 (if active 'powerline-active1 'powerline-inactive1))
+                            (face2 (if active 'powerline-active2 'powerline-inactive2))
+                            (separator-left (intern (format "powerline-%s-%s"
+                                                            (powerline-current-separator)
+                                                            (car powerline-default-separator-dir))))
+                            (separator-right (intern (format "powerline-%s-%s"
+                                                             (powerline-current-separator)
+                                                             (cdr powerline-default-separator-dir))))
+                            (lhs (list (powerline-raw "%*" face0 'l)
+                                       (powerline-buffer-size face0 'l)
+                                       (powerline-buffer-id `(mode-line-buffer-id ,face0) 'l)
+                                       (powerline-raw " ")
+                                       (funcall separator-left face0 face1)
+                                       (powerline-narrow face1 'l)
+                                       (powerline-vc face1)))
+                            (center (list (powerline-raw global-mode-string face1 'r)
+                                          (powerline-raw "%4l" face1 'r)
+                                          (powerline-raw ":" face1)
+                                          (powerline-raw "%3c" face1 'r)
+                                          (funcall separator-right face1 face0)
+                                          (powerline-raw " ")
+                                          (powerline-raw "%6p" face0 'r)
+                                          (powerline-hud face2 face1)
+                                          ))
+                            (rhs (list (powerline-raw " " face1)
+                                       (funcall separator-left face1 face2)
+                                       (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                         (powerline-raw erc-modified-channels-object face2 'l))
+                                       (powerline-major-mode face2 'l)
+                                       (powerline-process face2)
+                                       (powerline-raw " :" face2)
+                                       (powerline-minor-modes face2 'l)
+                                       (powerline-raw " " face2)
+                                       (funcall separator-right face2 face1)
+                                       ))
+                            )
+                       (concat (powerline-render lhs)
+                               (powerline-fill-center face1 (/ (powerline-width center) 2.0))
+                               (powerline-render center)
+                               (powerline-fill face1 (powerline-width rhs))
+                               (powerline-render rhs)))))))
+  (powerline-right-theme)
+  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit which-key use-package spacemacs-theme powerline monokai-theme dracula-theme deft counsel beacon avy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
